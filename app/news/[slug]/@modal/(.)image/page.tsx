b28d17038b8edfeef68ebbe53c49f645b2699466
prontoTheLegend <anasprog8@gmail.com>
2025-09-13 12:32:53 +0300

    navigating to previous page

--- app/news/[slug]/@modal/(.)image/page.tsx
+++ app/news/[slug]/@modal/(.)image/page.tsx
@@ -1,7 +1,8 @@
+'use client';
+
 import { Params } from "next/dist/server/request/params";
 import { DUMMY_NEWS } from "../../../../../news-dummy-data";
-import { notFound } from "next/navigation";
-
+import { notFound , useRouter} from "next/navigation";
 
 export default function ImagePage({params}:{params:Params}){
 
@@ -12,9 +13,11 @@ export default function ImagePage({params}:{params:Params}){
         notFound();
     }
 
+    const router = useRouter();
+
     return (
         <>
-            <div className="">
+            <div className="" onClick={router.back} >
                 <h1 className="">Intercepting route</h1>
                 <img src={`/images/news/${newsItem.image}`} alt={newsItem.title} className="" />
             </div>
