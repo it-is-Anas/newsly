7b59d0218cdf00abf8cfe2dd7c11dc4db96bc038
prontoTheLegend <anasprog8@gmail.com>
2025-09-13 14:05:56 +0300

    adding middleware

---
+++ middleware.ts
@@ -0,0 +1,11 @@
+import { NextResponse } from "next/server";
+
+
+export function middleware(requst: Request){
+    console.log('from middleware');
+    return NextResponse.next();
+}
+
+export const config = {
+    matcher: '/news',
+};
