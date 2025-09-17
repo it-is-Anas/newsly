47c4dba5df0652efa4237b4d8e4b39f988ba8e5b
prontoTheLegend <anasprog8@gmail.com>
2025-09-18 01:33:33 +0300

    Fetching data from server side

--- components/client-news-list.tsx
+++ components/client-news-list.tsx
@@ -1,26 +1,12 @@
-'use client';
-
-import { useEffect, useState } from "react";
 import NewsList from "./news-list";
 import { News } from "../news-dummy-data";
 
 
-export default function ClientNewsItems(){
-    const [news,setNews] = useState<News[]>([]);
-    useEffect(()=>{
-        
-        const fun = async ()=>{
-            try{
-                const back = await fetch('http://localhost:5000/news');
-                if(back.ok){
-                    const response = await back.json();
-                    setNews(response);
-                }
-            }catch(err){
-                console.log(err);
-            }
-        };
-        fun();
-    },[]);
-    return <NewsList news={news as News[]} />
+export default async function ClientNewsItems(){
+
+    const response = await fetch('http://localhost:5000/news');
+    const news = await response.json();
+    console.log(news);
+
+    return <NewsList news={news} />
 }
